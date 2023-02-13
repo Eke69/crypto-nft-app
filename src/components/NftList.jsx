c1d33c61788de902f558b978e0931f87c4c9e50b
Eke69 <e.kelechi93@gmail.com>
2023-02-13 11:46:19 +0100

    more fixes

--- src/components/NftList.jsx
+++ src/components/NftList.jsx
@@ -13,7 +13,8 @@ function formattedTime (time){
     return time.toLocaleTimeString([], {
     hour: '2-digit',
     minute: '2-digit',
-    second: '2-digit'
+    second: '2-digit',
+    hour12: false
   });
 }
 
